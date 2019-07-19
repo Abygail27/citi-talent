1246d766a72c7a05522273307b1705fa4002fd29
Abygail27 <abygaailrrocha@gmail.com>
2019-07-19 14:38:57 -0500

    login

--- citi/src/views/Login.js
+++ citi/src/views/Login.js
@@ -7,14 +7,19 @@ class Login extends Component {
   render () {
     return (
       <div id='background-login' className='row'>
-     
-     
-      <div id='figure'>
-        <figure></figure>
+      <div id='name-logo'>
+       
        </div>
-
-       <div className = "last"></div>
+        <figure>
+        </figure>
+        <article id='input-login'>
+          <input className='input-field col s8'  type='number' placeholder='Teléfono'></input>
+          <input className='input-field col s8' type='password' placeholder='Contraseña'></input>
+        </article>
+        <article>
+          <button className='login'><NavLink className='grey-text text-darken-4 font-nav' to='/home'>Iniciar Sessión</NavLink></button>
          
+        </article>
       </div>
     )
   }
