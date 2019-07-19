c629b076a5d491925096f6ee3108b151098ccaf2
AndreaMyshkn <atrkvsk@hotmail.com>
2019-07-19 01:50:48 -0500

    mando redux funcionando y conectado, falta maquetar

--- citi/src/views/Home.js
+++ citi/src/views/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {allContactsList} from '../actions'
+import { bindActionCreators } from 'redux'
+
+
+class Home extends Component {
+  componentWillMount =()=>{
+    this.props.allContactsList()
+  }
+  render(){
+    console.log(this.props)
+        return (
+            <div className="">
+               
+                 <div>Hola</div>
+            </div>
+        )
+    }
 }
 
-export default Home
+const mapStateToProps=(state)=>{
+       
+  return{
+  contacts:state.contacts
+  }
+
+}
+// This component dispatch an action to store
+const mapDispatchToProps = (dispatch) => {
+return bindActionCreators({ allContactsList}, dispatch)
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps) ( Home)
