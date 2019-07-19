5e7240bea517fedbec6359613fe556c0df0f172b
Karen Molina <milhadasdepapel@gmail.com>
2019-07-19 10:22:29 -0500

    maquetado de calculadora y home

--- citi/src/components/HistoryInformation.js
+++ citi/src/components/HistoryInformation.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import '../index.css'
 
 const HistoryInformation = (props) => {
@@ -12,29 +13,58 @@ const HistoryInformation = (props) => {
         </div>
         <div id='img-target' />
       </article>
-      <article id='button-container'>
-        <button className='btn-floating btn-large waves-effect waves-light' />
-        <button className='btn-floating btn-large waves-effect waves-light' />
-        <button className='btn-floating btn-large waves-effect waves-light' />
-        <button className='btn-floating btn-large waves-effect waves-light' />
-      </article>
+      <div id='button-container'>
+        <article >
+          <button className='btn-floating btn-large waves-effect waves-light envio'><NavLink className='grey-text text-darken-4 font-nav' to='/calculator' /></button>
+          <button className='btn-floating btn-large waves-effect waves-light pagos' />
+          <button className='btn-floating btn-large waves-effect waves-light ahorro' />
+          <button className='btn-floating btn-large waves-effect waves-light tarjeta' />
+          <div id='button-link'>
+            <span>Envíos</span>
+            <span>Pagos</span>
+            <span>Ahorro</span>
+            <span>Tarjeta</span>
+          </div>
+        </article>
+      </div>
 
       {props.users && props.users.length > 0
         ? props.users.map(item => (
           <section id='container-mov'>
-            <h6 id='mov'>Movimientos</h6>
+            <h6 id='mov'><strong>Movimientos</strong></h6>
             <div id='container-history'>
               <div id='id1'>
                 <h6>{item.history.history1.sendto}</h6>
-                <span> {item.history.history1.day}</span>
-                <span>${item.history.history1.amount}</span>
+                <span> Fecha: {item.history.history1.day}</span>
+                <span>Monto: ${item.history.history1.amount}</span>
               </div>
               <div>
                 <h6>{item.history.history2.sendto}</h6>
-                <span> {item.history.history2.day}</span>
-                <span>${item.history.history2.amount}</span>
+                <span> Fecha: {item.history.history2.day}  </span>
+                <span> Monto: ${item.history.history2.amount}</span>
+              </div>
+              <div>
+                <h6>{item.history.history3.sendto}</h6>
+                <span> Fecha: {item.history.history3.day}  </span>
+                <span> Monto: ${item.history.history3.amount}</span>
+              </div>
+              <div>
+                <h6>{item.history.history4.sendto}</h6>
+                <span> Fecha: {item.history.history4.day}  </span>
+                <span> Monto: ${item.history.history4.amount}</span>
+              </div>
+              <div>
+                <h6>{item.history.history5.sendto}</h6>
+                <span> Fecha: {item.history.history5.day}  </span>
+                <span> Monto: ${item.history.history5.amount}</span>
+              </div>
+              <div>
+                <h6>{item.history.history6.sendto}</h6>
+                <span> Fecha: {item.history.history6.day}  </span>
+                <span> Monto: ${item.history.history6.amount}</span>
               </div>
             </div>
+
           </section>
 
         )) : null
--- citi/src/views/Calculator.js
+++ citi/src/views/Calculator.js
@@ -1,21 +1,68 @@
-
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {exchangeRate} from '../actions'
 import { bindActionCreators } from 'redux'
+import '../calculator.css'
 
 
 class Calculator extends Component {
+
+handleChange = e => {
+  this.setState({value:e.target.value})
+}
+
   componentWillMount =()=>{
     this.props.exchangeRate()
   }
+  
   render(){
-    console.log(this.props.citi.exchange)
+   
         return (
-            <div className="">
-               
-                 <div>hola</div>
-            </div>
+          <div id='background-calculator' className='row'>
+          <div id='name-logo'>
+        <div className = "money-exchange"></div>
+    
+          </div>
+           
+            <article id='information-contact'>
+            Envía hasta $500 dólares sin comisión.
+            </article>
+
+           <div className = "money-exchange">
+            <p> Hoy, tipo de cambio: <br></br>$18.97 pesos mexicano.</p>
+          <img src = "https://vidatel.com.br/wp-content/uploads/2017/02/money-coin-or-button-with-dollar-sign-in-black-circle.png" width = "50px"></img>
+             
+           </div>
+
+          <div className = "movements">
+            <p>Movimientos</p>
+         <input className = "movement-us"type = "text" placeholder = "cantidad enviada" Onchange = {this.handleChange}></input>
+         <input className = "movement-mx"type = "text" placeholder = "cantidad recibida" ></input>
+          </div>
+         
+         <div className = "transfer">
+         <div className="container">
+        <div className="row">
+          <div className="col s12 board">
+            <table id="simple-board">
+               <tbody style = {{
+                 fontWeight: '500',
+                 fontSize: '14px'
+               }}> Resumen
+
+           <ul>Cantidad Enviada</ul>
+           <ul>Comisiones</ul>
+           <ul>Total</ul>
+               </tbody>
+             </table>
+             <button id = "send-transfer">Enviar</button>
+          </div>
+        </div>
+      </div>
+         </div>
+
+          </div>
+
         )
     }
 }
